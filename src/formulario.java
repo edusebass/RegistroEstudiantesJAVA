f12bfcd2dee51d8e359af4167bde1914451770d2
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-07-14 11:17:09 -0500

    Creacion de panel

---
+++ src/formulario.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class formulario {
+    private JPanel rootpanel;
+    private JTextField codigotextField1;
+    private JTextField cedulatextField2;
+    private JTextField nombrestextField3;
+    private JTextField textField4;
+    private JCheckBox rojoCheckBox;
+    private JCheckBox verdeCheckBox;
+    private JCheckBox ningunoCheckBox;
+    private JRadioButton siRadioButton;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JButton cargarDatosDesdeElButton;
+    private JButton guardarDatosEnDiscoButton;
+    private JTextPane textPane1;
+    private JButton verAnteriorRegistroButton;
+    private JButton verSiguienteRegistroButton;
+    private JTextField textField5;
+    private JRadioButton noRadioButton;
+}
